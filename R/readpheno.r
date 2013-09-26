02dd11e23a01dc30c108c89e8930f9f85c3311a1
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2013-09-26 18:23:08 -0500

    placeholders are cruft for now

--- R/readpheno.r
+++
@@ -1,7 +0,0 @@
-##
-readpheno <- function(phenofile){
-    pheno <- read.table(phenofile,as.is=T,header=F)
-    names(pheno) <- c("FID","IID","Status")
-    rownames(pheno) <- pheno$IID
-    return(pheno)
-}
