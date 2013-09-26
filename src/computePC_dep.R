4f1836d512f8237df39675201dae61e82d830560
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2013-09-26 16:50:18 -0500

    removed deprecated code

--- src/computePC_dep.R
+++
@@ -1,7 +0,0 @@
-computePC <-
-function(fullheader,idfile=idfile,gctaname="gcta")
-{
-  runPC = paste(gctaname , ' --grm ' , fullheader , ' --keep ' , idfile , ' --pca 10 --out ' , fullheader,sep="")
-  print(runPC)
-  system(runPC)
-}
