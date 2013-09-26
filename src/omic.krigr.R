2496225cec2a2a6ccf036d4427c807d10de57c6f
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2013-09-26 13:53:15 -0500

    added some working code

--- src/omic.krigr.R
+++ src/omic.krigr.R
@@ -6,6 +6,7 @@ library(SNPRelate)
 library(Rcpp)
 library(RcppEigen)
 library(inline)
+library(doMC)
 
 ## functions
 
@@ -15,6 +16,7 @@ gdsFile <- "wtcct1d.gds"
 bedFile <- "data/T1DCC.bed"
 bimFile <- "data/T1DCC.bim"
 famFile <-"data/T1DCC.fam"
+ncore <- 12
 
 ## INPUTS 
 ## convert plink file to GDS
@@ -24,10 +26,48 @@ genofile <- openfn.gds(gdsFile)
 X <- read.gdsn(index.gdsn(genofile, "genotype"))
 Y <- read.gdsn(index.gdsn(genofile, "sample.annot"))$phenotype
 
+## TODO:: factor phenotype handling into GDS structure
+sample.id <- read.gdsn(index.gdsn(genofile, "sample.id"))
+pheno.df <- dataframe(test=Y)
+rownames(pheno.df) <- sample.id
+
 ## compute GRM matrix (or read in?)
 source('src/rcppcormat.r')
-grm <- rcppcormat(t(X))
+## center genotype matrix
+X.center <- sweep(X, 2, colMeans(X), "-")
+grm <- rcppcormat(t(X.center))
 
-## cross validation
+## PARALLEL KRIGING
+source('src/okriging.R')
+corlist <- list()
+corlist[[1]] <- grm
+
+
+## setup the cores
+registerDoMC(cores = ncore)
+
+## parallel cross validation (n-fold)
+idlength <- length(sample.id)
+g <- 1:ncore
+groupid <- sample(g,idlength,replace=T)
+newiddata <- data.frame(groupid, sample.id)
+colnames(newiddata) <- c("group.id","sample.id")
+
+## run kriging in parallel
+p <- foreach(i=1:ncore, .combine=rbind) %dopar% {
+  
+  test.set = newiddata$sample.id[newiddata$group.id == i]
+  train.set = newiddata$sample.id[!(newiddata$sample.id %in% test.set)]
+  
+  okriging(idtest=test.set,
+           idtrain=train.set,
+           corlist=corlist,
+           H2vec=0.5, #?
+           pheno=pheno.df,
+           phenoname="test")
+}
+
+
+
 
 
