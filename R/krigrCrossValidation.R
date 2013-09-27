32b778612724b2929b202d2c5cbb2a956eea0f46
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2013-09-27 11:33:42 -0500

    added docs for the cross validation placeholder function

--- R/krigrCrossValidation.R
+++ R/krigrCrossValidation.R
@@ -1,5 +1,25 @@
-## n-core, n-fold cross validation routine for kriging
-## TODO:: separate distinction between level of parallelism and fold level
+#' Run n-fold cross validated kriging across n-cores.
+#'
+#' For now, this is a placeholder function. What is really needed is a function
+#' which can run across multiple cores with an arbitrary number of test/train
+#' groups, i.e. decouple the n-core and n-fold variables. At its core,
+#' this function calls \code{\link{okriging}}.
+#'
+#' @param n.core The number of cores used and test sets the data will be split into.
+#' @param corlist A list of correlation matrices. Colnames and rownames must be
+#'   sample IDs, and must match betweenn matrices.
+#' @param pheno.df A data frame of sample IDs and their corresponding phenotypes. The
+#'   data frame must have rownames() set to sample IDs.
+#' @param pheno.name The name of the column in the pheno.df which is the phenotype
+#'   being predicted.
+#' 
+#' @return A data frame with a column for each: sample IDs, observed phenotypes,
+#'   and predicted phenotypes.
+#' 
+#' @include R/okriging.R
+#'
+#' @keywords prediction, cross validation
+#' @export
 krigr_cross_validation <- function(n.cores, corlist, pheno.df, pheno.name) {
   ## TODO:: handling internal package references
   source('R/okriging.R')
