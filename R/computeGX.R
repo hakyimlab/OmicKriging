753a8514f24fb4443ed60a15c586c086e82e6aba
vtrubets <trubetskoy.vasa@gmail.com>
2013-10-01 17:15:14 -0500

    Swapped out gzip file writing for GCTA style binary file formats. Added documentation

--- R/computeGX.R
+++ R/computeGX.R
@@ -1,14 +1,25 @@
 #' Compute gene expression correlation matrix.
 #'
+#' This function computes a gene expression correlation matrix given a file of
+#' transcript expression levels for each sample in the study. It returns a
+#' correlation matrix with rownames and colnames as sample IDs.
 #'
+#' @param genefile Path to gene expression file.
+#' @param outFilePrefix File path prefixes for outputting GCTA style binary
+#'   correlation matrices.
 #'
+#' @return Returns a correlation matrix of (N-samples x N-samples), with
+#'   rownames and colnames as sample IDs.
 #'
+#' @include R/grm_io.R
 #'
-#'
-#'
-#'
-computeGX <- function(genefile,outheader,idfile=NULL) {
-  genedata = read.delim(genefile,sep="",as.is=T,header=T)
+#' @export
+computeGX <- function(genefile, outFilePrefix, idfile=NULL) {
+  
+  source('R/grm_io.R')
+
+  ## data input
+  genedata = read.delim(genefile, sep="", as.is=T, header=T)
   if(!is.null(idfile)) {
     iddata = read.table(idfile,header=T,as.is=T)
     genedata = merge(iddata,genedata,by.x=c("FID","IID"),by.y=c("FID","IID"))
@@ -19,15 +30,11 @@ computeGX <- function(genefile,outheader,idfile=NULL) {
   ## Standardize this matrix?
   if(sum(is.na(genemat))>0) print("Impute first") ## TODO: impute with the script
   cormat = cor(t(genemat))
-  ## format matrix into linear dataframe (GCTA format)
-  ng = ncol(genemat)
-  cordata = cbind(c(col(cormat)),c(row(cormat)),ng,c(cormat))
-  cordata = subset(cordata,cordata[,1]>=cordata[,2])
+  colnames(cormat) <- cordata.id[,2]
+  rownames(cormat) <- cordata.id[,2]
+
   ## write to disk
-  ## replace this with write to GCTA style correlation matrix file
-  write.table(cordata,file=paste(outheader , ".grm",sep=""),col.names=F,row.names=F,quote=F)
-  system(paste("gzip " , outheader , ".grm ",sep=""))
-  ## write id file to disk
-  write.table(cordata.id,file=paste(outheader , ".grm.id",sep=""),col.names=F,row.names=F,quote=F)
+  writeGRMBin(X = cormat, prefix = outFilePrefix, n.snps = length(genemat[1,])) 
+
   return(cormat)
 }
