b0c1ea677a6c966e28d20114f6a7d2f0526ac955
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2014-06-04 16:03:20 -0500

    untested update

--- R/correlation_matrices.R
+++ R/correlation_matrices.R
@@ -17,7 +17,6 @@
 #'
 #' @import gdsfmt
 #' @import SNPRelate
-#' @import RcppEigen
 #'
 #' @keywords input, GRM
 #' @export
@@ -186,47 +185,8 @@ make_PCs_irlba <- function(X, n.top = 2) {
 #'
 #' @return An n x n correlation matrix.
 #'
-#' @importFrom inline cxxfunction
-#' @import Rcpp
-#'
 #' @keywords c++, performance, correlation matrix
 rcppcormat <- function(snpmat){
-    ## rcpp
-    crossprodCpp <- '
-    using Eigen::Map;
-    using Eigen::MatrixXd;
-    using Eigen::Lower;
-
-    // set constants
-
-    const Map<MatrixXd> A(as<Map<MatrixXd> >(AA));
-    const int n(A.cols());
-
-    // create n x n matrix
-
-    MatrixXd AtA(MatrixXd(n, n)
-
-    // zero out the matrix
-
-    .setZero()
-
-    // treat what follows as symmetric matrix, use lower
-
-    .selfadjointView<Lower>()
-
-    // sum of B + AA
-
-    .rankUpdate(A.adjoint()));
-
-    // return
-
-    return wrap(AtA);
-    '
-  
-    ## compile the cross product function
-    cpcpp <- cxxfunction(signature(AA="matrix"), crossprodCpp,
-    plugin="RcppEigen", verbose=FALSE)
-
     cormat <- cpcpp(snpmat)
 
     ## post work
